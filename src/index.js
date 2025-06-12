01fab785e257bc3a323fe4f630f215e1f83db9ab
Kaio Gomes <kaiogomes.kor@gmail.com>
2025-06-11 21:41:36 -0300

    v4: habilitando CORS para todas as rotas

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ conexao.on("error", (erro) => { console.error("Erro na conexão", erro) })
 conexao.once("open", () => console.log("Conexão feita com sucesso!"))
 
 const app = express()
-app.use(cors({ origin: 'https://refuge-uyzc.vercel.app' }))
+app.use(cors({ origin: '*' }))
 routes(app)
 
 export default app
